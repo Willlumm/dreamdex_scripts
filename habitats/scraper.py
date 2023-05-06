b0ecd36fe29c672a3533bb2381890ab6583777eb
Willlumm <william.lumme@gmail.com>
2023-05-06 22:36:24 +0100

    Finish scraper

--- habitats/scraper.py
+++ habitats/scraper.py
@@ -4,12 +4,15 @@ from requests_html import HTMLSession
 
 data = []
 session = HTMLSession()
-for i in range(387, 388):
-    result = session.get('https://www.theworldofpokemon.com/entryPages/entryPage_387.html')
+print("Scraping...")
+for i in range(0, 1009):
+    result = session.get("https://www.theworldofpokemon.com/entryPages/entryPage_f{i}.html")
     result.html.render()
     species = result.html.find("#entry-name", first=True).text
     habitat = result.html.find("#Habitat", first=True).text
     data.append((species, habitat))
+    if i % 100 == 0:
+        print(f"{i} of 1009 complete...")
 session.close()
 df = pd.DataFrame(data, columns=["species", "habitat"])
 df.to_csv("habitats/habitats.csv")
